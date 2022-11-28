14457765c0941eda0e905800a34dc592a10ea80f
Shubhankar395 <90629016+Shubhankar395@users.noreply.github.com>
2022-11-28 19:43:43 +0530

    blog tiny support

--- src/component/layout/landing/chairman-corner/chairman.js
+++ src/component/layout/landing/chairman-corner/chairman.js
@@ -19,13 +19,18 @@ const ChairMan = () => {
                     style={{ gap: ".6rem" }}
                   >
                     <img src={newsLogo} alt="logo" />
-                    <a href={"https://5feb.xyz/blog/637d00aa63efe5df8f6fc963"} className="news-font pt-4">
-                     Smart TV & DTH Manufecturer Vlive is one of the largest and trustworthy manufacturers of home appliances.
+                    <a
+                      href={"https://5feb.xyz/blog/637d00aa63efe5df8f6fc963"}
+                      className="news-font pt-4"
+                    >
+                      Smart TV & DTH Manufecturer Vlive is one of the largest
+                      and trustworthy manufacturers of home appliances.
                     </a>
                   </div>
                   <div
                     className="d-flex align-items-center"
                     style={{ gap: ".6rem" }}
+                  
                   >
                     <img src={newsLogo} alt="logo" />
                     <h6 className="news-font pt-4">
@@ -55,10 +60,12 @@ const ChairMan = () => {
                     <h4>Chairman's Corner</h4>
                     <div className="py-2">
                       <h4 className="pt-3 font-size">
-                      “Everyone starts business to gain but we started our Vlive India to give and gain both. 
-Because if you are doing a business for only yourself, you are a good businessman but if you are doing it for the entire nation… then that makes you a good human being and a good businessman as well.”
- 
-
+                        “Everyone starts business to gain but we started our
+                        Vlive India to give and gain both. Because if you are
+                        doing a business for only yourself, you are a good
+                        businessman but if you are doing it for the entire
+                        nation… then that makes you a good human being and a
+                        good businessman as well.”
                       </h4>
                     </div>
                     <h6 className="pt-5 padding-bottom">Manish Sharma</h6>
@@ -71,7 +78,11 @@ Because if you are doing a business for only yourself, you are a good businessma
                 </div>
               </div>
               <div className="col-lg-6 bg-image hover-zoom mt-5 mt-lg-0">
-                  <img src={chairman} alt="chairman" className="chairman-img-fluid" />
+                <img
+                  src={chairman}
+                  alt="chairman"
+                  className="chairman-img-fluid"
+                />
               </div>
             </div>
           </div>
